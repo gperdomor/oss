9ed0f3aea159352f19474afa6fdb5ad12587310c
Gustavo Perdomo <gperdomor@gmail.com>
2026-08-01 22:37:22 -0400

    chore: update to nx 23.1.1 (#1550)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,11 @@
 import nx from '@nx/eslint-plugin';
 
 export default [
-  {
-    files: ['**/*.json'],
-    // Override or add rules here
-    rules: {},
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser'),
-    },
-  },
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc', '**/vitest.config.*.timestamp*', '**/vite.config.*.timestamp*'],
+    ignores: ['**/dist', '**/out-tsc', '**/vitest.config.*.timestamp*'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -37,5 +29,13 @@ export default [
     files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
     // Override or add rules here
     rules: {},
+  },
+  {
+    files: ['**/*.json'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
   },
 ];
--- vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    projects: ['packages/*/vitest.config.{mjs,js,ts,mts}', 'packages/class-variance-builder/vite.config.mts'],
-  },
-});
